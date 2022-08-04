a6f848e94bd17023227110640ec09832b8765e28
Konrad Markowski <konrad.markowski.1993@gmail.com>
2022-08-04 16:51:32 +0200

    initial project configuration

---
+++ src/test/java/pl/markowski/konrad/app/artgallery/ArtGalleryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.markowski.konrad.app.artgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtGalleryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
